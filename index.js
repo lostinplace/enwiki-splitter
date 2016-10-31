7dbf78b72855774a743b1d820da1bafe83584feb
Chris Wheeler <cwwhee@amazon.com>
2016-10-31 17:12:08 -0400

    added pattern matching

--- index.js
+++ index.js
@@ -8,16 +8,33 @@ var saxParser  = sax.createStream(true);
 var streamer   = new saxpath.SaXPath(saxParser, '/mediawiki/page');
 var outFolder = process.argv[2]
 debugger
+var pattern = RegExp(process.argv[3])
+var counter = 0;
+var lastPrinted = 0;
+
+function updateSkip(){
+  console.log(`skipped: ${counter - lastPrinted}, continuing from ${counter}`);
+  lastPrinted = counter;
+}
 
 streamer.on('match', xml => {
+  counter++;
   parseString(xml, (err,result) => {
-    if(!result.page.redirect){
+    let hasRedirect = result.page.redirect
+    let text = !hasRedirect && result.page.revision[0].text[0]
+    let matches = text && text._.match(pattern)
+    if(matches){
       var title = result.page.title[0];
       var id = result.page.id[0];
       var outFileName = id;
       var outPath = `${outFolder}/${outFileName}.xml`;
       fs.writeFileSync(outPath, xml);
+      updateSkip()
       console.log(`wrote: "${title}" to "${outPath}" `)
+    } else {
+      if((counter - lastPrinted) > 1000) {
+        updateSkip()
+      }
     }
   });
 });
